ef303f6d33b5756640e0031272b66f87623eb9c5
aman2656 <42776181+aman2656@users.noreply.github.com>
2020-08-16 13:24:01 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,11 @@
+mkdir -p ~/.streamlit/
+echo “\
+[general]\n\
+email = \”amangupta2106@gmail.com\”\n\
+“ > ~/.streamlit/credentials.toml
+echo “\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+“ > ~/.streamlit/config.toml
